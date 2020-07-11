a2b391152db428ff44a357754b78f9b1c0817c58
Mihkel Kuusküll <mihkel1994@gmail.com>
2020-07-11 10:41:31 +0300

    Can put cells on map

---
+++ src/components/cell/cell.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './cell.css';
+
+type Props = {
+    x: number;
+    y: number;
+    size: number;
+};
+
+export default function Cell({ x, y, size }: Props) {
+    return (
+        <div className="cell" style={{
+            left: `${size * x + 1}px`,
+            top: `${size * y + 1}px`,
+            width: `${size - 1}px`,
+            height: `${size - 1}px`,
+        }}/>
+    );
+}
--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './game.css';
+import Cell from '../cell/cell';
 
 export default function Game() {
+    const cellSize = 20;
+    const height = 600;
+    const width = 800;
+    const rows = height / cellSize;
+    const columns = width / cellSize;
+    let boardRef: any;
+    const board = createEmptyBoard();
+    const [cells, setCells] = useState<{ x: number, y: number }[]>([]);
+
+    function createEmptyBoard() {
+        let board: number[][] = [];
+        for (let y = 0; y < rows; y++) {
+            board[y] = [];
+            for (let x = 0; x < columns; x++) {
+                board[y][x] = 0;
+            }
+        }
+
+        return board;
+    }
+
+    function onUserClick({ clientX, clientY }: { clientX: number, clientY: number }) {
+        const elemOffset = getElementOffset();
+        const offsetX = clientX - elemOffset.x;
+        const offsetY = clientY - elemOffset.y;
+        
+        const x = Math.floor(offsetX / cellSize);
+        const y = Math.floor(offsetY / cellSize);
+
+        if (x >= 0 && x <= columns && y >= 0 && y <= rows) {
+            board[y][x] = Boolean(board[y][x]) === true ? 0 : 1;
+        }
+
+        setCells(makeCells());
+    }
+
+    function getElementOffset() {
+        const rect = boardRef?.getBoundingClientRect();
+        const doc = document.documentElement;
+
+        return {
+            x: (rect.left + window.pageXOffset) - doc.clientLeft,
+            y: (rect.top + window.pageYOffset) - doc.clientTop,
+        };
+    }
+
+    function makeCells() {
+        let cells = [];
+        for (let y = 0; y < rows; y++) {
+            for (let x = 0; x < columns; x++) {
+                if (board[y][x]) {
+                    cells.push({ x, y });
+                }
+            }
+        }
+
+        return cells;
+    }
+
     return (
         <div>
-            <div className="Board" />
+            <div className="Board" style={{ width, height, backgroundSize: `${cellSize}px ${cellSize}px`}} onClick={onUserClick} ref={(n) => { boardRef = n; }}>
+                {cells.map(cell => (
+                    <Cell size={cellSize} x={cell.x} y={cell.y} />
+                ))}
+            </div>
+            <div>
+                Control go in here
+            </div>
         </div>
     );
 }
