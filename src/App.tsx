c587678b30f942ed0101033ec887fde08799d0ac
Mihkel Kuusküll <mihkel@coolbet.com>
2020-07-11 03:07:13 +0300

    start ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Game from './components/game/game';
- 
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Game></Game>
         <p>
-          Edit <code>src/App.js</code> and load to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
           className="App-link"
