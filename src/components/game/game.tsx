cfdf16692c583d1b67c097b6a0f85395ac513804
Luukuduja <siim_k@outlook.com>
2020-07-12 20:26:10 +0300

    html instructions buttons tutorial

--- src/components/game/game.tsx
+++ src/components/game/game.tsx
@@ -14,6 +14,10 @@ import {
 } from '../../services/cell';
 import sampleSize from 'lodash/sampleSize';
 import logo from '../art/logo.png';
+import virus from '../board-cell/virus.svg';
+import antibody from '../board-cell/antibody.svg';
+import deadmatter from '../board-cell/deadmatter.svg';
+import superspreader from '../board-cell/superspreader.svg';
 /* import soundfile from './background.mp3'; */
 import Sound from 'react-sound';
 
@@ -324,8 +328,53 @@ export default function Game() {
                         </button>}
                     </>
                 )}
+            </div>    
+            <div className="Tutorial">
+
+                <h3>Instructions</h3>
+
+                <table>
+
+                    <tr>
+                        <th></th>
+                        <th>Type</th> 
+                        <th>Description</th>
+                    </tr>
+
+                    <tr>
+                        <td><img id="CellPicture" src={virus} alt="Virus"/></td>
+                        <td>Virus</td>
+                        <td id="description">This little guy spreads if it has 2 or 3 infected cells around it. Also if a healthy cell is surrounded by 3 infected cells it becomes infected.</td>
+                    </tr>
+
+                    <tr>
+                        <td><img id="CellPicture" src={antibody} alt="Antibody"/></td>
+                        <td>Antibody</td>
+                        <td>This little guy is a hero. Antibodies kill virus cells if they come into contact with eachother.</td>
+                    </tr>
+
+                    <tr>
+                        <td><img id="CellPicture" src={deadmatter} alt="Dead matter"/></td>
+                        <td>Dead cell</td>
+                        <td>This cell has died and is inert. Dead cells can't turn into a virus</td>
+                    </tr>
+
+                    <tr>
+                        <td><img id="cellPicture"src={superspreader} alt="Super Spreader"/></td>
+                        <td>Super spreader</td>
+                        <td>This cell is a superspreader, it explodes and spreads the virus everywhere around it.</td>
+                    </tr>
+
+                 </table>
+
             </div>
-            <Sound
+
+            <div className="Authors">
+                <p>created by not mario brothers
+                </p>
+            </div>
+            
+            <Sound loop
                 url={x}
                 playStatus="PLAYING"
             />
